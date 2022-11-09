c4ce1d82a696f4d044f9b6d4b0705acada07f1d8
Joonwoo Park <93jpark@gmail.com>
2022-11-09 14:18:24 +0900

    init project

---
+++ src/main/java/Tamanegiseoul/comeet/CoMeetApplication.java
@@ -0,0 +1,13 @@
+package Tamanegiseoul.comeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoMeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoMeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tamanegiseoul/comeet/CoMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package Tamanegiseoul.comeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoMeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
