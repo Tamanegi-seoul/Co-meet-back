09e1516042b2cd8b762757ecabc28081dcdb394f
Joonwoo Park <93jpark@gmail.com>
2022-11-10 16:35:58 +0900

    implemented test code for finding user preferred stack

--- src/test/java/Tamanegiseoul/comeet/UserServiceTest.java
+++ src/test/java/Tamanegiseoul/comeet/UserServiceTest.java
@@ -1,6 +1,7 @@
 package Tamanegiseoul.comeet;
 
 import Tamanegiseoul.comeet.domain.Users;
+import Tamanegiseoul.comeet.domain.enums.TechStack;
 import Tamanegiseoul.comeet.repository.UserRepository;
 import Tamanegiseoul.comeet.service.UserService;
 
@@ -13,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,11 +23,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class UserServiceTest {
 
-    @Autowired
-    UserService userService;
-
-    @Autowired
-    UserRepository userRepository;
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
 
     @Test
     public void 단일_유저_생성() throws Exception {
@@ -41,5 +41,31 @@ public class UserServiceTest {
         // then
         Users findUser = userService.findUserByNickname("test_user");
         Assert.assertEquals(findUser.getEmail(), "testuser@gmail.com");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void 유저_이메일_중복검사() {
+
+    }
+
+    @Test
+    public void 기술스택_세팅() {
+        // given
+        Users newUser = Users.builder()
+                .nickname("test_user")
+                .email("testuser@gmail.com")
+                .password("password")
+                .build();
+        userService.registerUser(newUser);
+
+        // when
+        userService.updatePreferStack(newUser.getId(), TechStack.JAVA, TechStack.R);
+
+        // then
+        List<TechStack> findStacks = userService.findPreferredStacks(newUser.getId());
+        for(TechStack ts : findStacks) {
+            log.info("TechStack : " + ts.name());
+        }
+        Assert.assertEquals(2, findStacks.size());
     }
 }
