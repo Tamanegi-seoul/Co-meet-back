aa2c4d1a0e4241aed45d1dcd9e7a73ea74c24334
Joonwoo Park <93jpark@gmail.com>
2022-11-10 16:46:22 +0900

    added field for DuplicateReousrceException with field type specifying

--- src/main/java/Tamanegiseoul/comeet/domain/exception/DuplicateResourceException.java
+++ src/main/java/Tamanegiseoul/comeet/domain/exception/DuplicateResourceException.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 @Getter
 public class DuplicateResourceException extends RuntimeException {
     @Getter
+    private String valueType;
     private String fieldValue;
 
-    public DuplicateResourceException(String fieldValue) {
-        super(String.format("%s is already exists", fieldValue));
+    public DuplicateResourceException(String valueType, String fieldValue) {
+        super(String.format("%s's %s is already exists", fieldValue, fieldValue));
         this.fieldValue = fieldValue;
+        this.valueType = valueType;
     }
 }
