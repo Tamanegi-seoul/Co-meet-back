e871688bad426d7aba582890d50b6533eda183fc
Joonwoo Park <93jpark@gmail.com>
2022-11-13 01:11:09 +0900

    edited builder with initializing

--- src/main/java/Tamanegiseoul/comeet/domain/Posts.java
+++ src/main/java/Tamanegiseoul/comeet/domain/Posts.java
@@ -1,7 +1,9 @@
 package Tamanegiseoul.comeet.domain;
 
 import Tamanegiseoul.comeet.domain.enums.ContactType;
+import Tamanegiseoul.comeet.domain.enums.RecruitStatus;
 import Tamanegiseoul.comeet.domain.enums.TechStack;
+import Tamanegiseoul.comeet.dto.post.request.UpdatePostRequest;
 import com.sun.istack.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -28,6 +30,9 @@ public class Posts {
 
     @NotNull
     private String title;
+
+    @NotNull
+    private RecruitStatus recruitStatus;
 
     @NotNull
     private Long recruitCapacity;
@@ -69,8 +74,9 @@ public class Posts {
     private LocalDate modifiedDate;
 
     @Builder
-    public Posts(String title, Long recruitCapacity, String contact, ContactType contactType, LocalDate startDate, Long expectedTerm, String content, Users poster, TechStack...tss) {
+    public Posts(String title, Long recruitCapacity, String contact, ContactType contactType, LocalDate startDate, Long expectedTerm, String content, Users poster) {
         this.title = title;
+        this.recruitStatus = RecruitStatus.RECRUIT;
         this.recruitCapacity = recruitCapacity;
         this.startDate = startDate;
         this.expectedTerm = expectedTerm;
@@ -78,24 +84,27 @@ public class Posts {
         this.poster = poster;
         this.contactType = contactType;
         this.contact = contact;
+        this.hits = 0L;
         this.createdDate = LocalDate.now();
-
-        for(TechStack stack : tss) {
-            this.designatedStack.add(StackRelation.builder()
-                    .post(this)
-                    .techStack(stack).build());
-        }
-
         this.modifiedDate = LocalDate.now();
     }
 
-    public static void addDesignateStack(TechStack ...ts) {
-
+    public void addDesignateStack(TechStack ts) {
+        this.designatedStack.add(StackRelation.builder()
+                .post(this)
+                .techStack(ts).build());
     }
 
-    public void updateDesignateStack(TechStack ...ts) {
-        //StackRelation newSt = StackRelation.createForUser(this, ts);
-        for(TechStack stack : ts) {
+//    public void updateDesignateStack(TechStack ...ts) {
+//        for(TechStack stack : ts) {
+//            this.designatedStack.add(StackRelation.builder()
+//                    .post(this)
+//                    .techStack(stack).build());
+//        }
+//    }
+
+    public void updateDesignateStack(List<TechStack> tsArr) {
+        for(TechStack stack : tsArr) {
             this.designatedStack.add(StackRelation.builder()
                     .post(this)
                     .techStack(stack).build());
@@ -115,9 +124,42 @@ public class Posts {
     public void updateContact(String newContact) { this.contact = contact; }
     public void updateCreatedDate() { this.createdDate = LocalDate.now(); }
     public void updateModifiedDate() { this.modifiedDate = LocalDate.now(); }
+    public void updateRecruitStatus(RecruitStatus rs) { this.recruitStatus = rs; }
 
     public void initDesignateStack() {
         this.designatedStack = new ArrayList<>();
     }
 
+    public void updatePost(UpdatePostRequest updatePost) {
+        this.title = updatePost.getTitle();
+        this.content = updatePost.getContent();
+        this.contactType = updatePost.getContactType();
+        this.contact = updatePost.getContact();
+        this.recruitStatus = updatePost.getRecruitStatus();
+        this.recruitCapacity = updatePost.getRecruitCapacity();
+        this.startDate = updatePost.getStartDate();
+        this.expectedTerm = updatePost.getExpectedTerm();
+    }
+
+    public PostEdit toPostEdit() {
+        return PostEdit.builder()
+                .contact(this.contact)
+                .contactType(this.contactType)
+                .content(this.content)
+                .createdDate(this.createdDate)
+                .expectedTerm(this.expectedTerm)
+                .hits(this.hits)
+                .recruitCapacity(this.recruitCapacity)
+                .recruitStatus(this.recruitStatus)
+                .startDate(this.startDate)
+                .title(this.title)
+                .build();
+    }
+
+    public String printout() {
+        return String.format("title: %s\n content: %s\n recruit status: %s\n recruit capacity: %s\n start date: %s\n" +
+                "contact: %s\n contact type: %s\n start date: %s\n expected term: %s\n created date: %s\n modified date: %s\n",
+                this.title, this.content, this.recruitStatus, this.recruitCapacity, this.startDate, this.contact,
+                this.contactType, this.startDate, this.expectedTerm, this.createdDate, this.modifiedDate);
+    }
 }
