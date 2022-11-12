1b989008f529bda7e4eaaad8bc18eb85e69b7f5c
Joonwoo Park <93jpark@gmail.com>
2022-11-13 01:10:30 +0900

    init dto directories

--- src/main/java/Tamanegiseoul/comeet/dto/post/request/UpdatePostRequest.java
+++ src/main/java/Tamanegiseoul/comeet/dto/post/request/UpdatePostRequest.java
@@ -1,2 +1,25 @@
-package Tamanegiseoul.comeet.dto.post.request;public class UpdatePostRequest {
+package Tamanegiseoul.comeet.dto.post.request;
+
+import Tamanegiseoul.comeet.domain.enums.ContactType;
+import Tamanegiseoul.comeet.domain.enums.RecruitStatus;
+import Tamanegiseoul.comeet.domain.enums.TechStack;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+public class UpdatePostRequest {
+    private String title;
+    private String content;
+    private ContactType contactType;
+    private String contact;
+    private RecruitStatus recruitStatus;
+    private Long recruitCapacity;
+    private LocalDate startDate;
+    private Long expectedTerm;
+    private List<TechStack> stacks = new ArrayList<>();
 }
