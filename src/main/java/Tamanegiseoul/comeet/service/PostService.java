f08e415a2bdc9ef6d921a3987dab3f548537490f
Joonwoo Park <93jpark@gmail.com>
2022-11-13 01:12:35 +0900

    edited method for update post and implemented TechStack exporting method

--- src/main/java/Tamanegiseoul/comeet/service/PostService.java
+++ src/main/java/Tamanegiseoul/comeet/service/PostService.java
@@ -1,19 +1,75 @@
 package Tamanegiseoul.comeet.service;
+import Tamanegiseoul.comeet.domain.PostEdit;
+import Tamanegiseoul.comeet.domain.Posts;
+import Tamanegiseoul.comeet.domain.enums.TechStack;
+import Tamanegiseoul.comeet.dto.post.request.UpdatePostRequest;
 import Tamanegiseoul.comeet.repository.CommentRepository;
+import Tamanegiseoul.comeet.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PostService {
 
     private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
     private final UserService userService;
     private final StackRelationService stackRelationService;
 
     @PersistenceContext
     EntityManager em;
+
+    @Transactional
+    public Long registerPost(Posts post) {
+        postRepository.save(post);
+        return post.getId();
+    }
+
+    /***********************
+     * UPDATE POST METHODS *
+     ***********************/
+
+    @Transactional
+    public void updatePost(Long id, UpdatePostRequest updatedPost) {
+        Posts findPost = postRepository.findOne(id);
+        findPost.updatePost(updatedPost);
+        findPost.initDesignateStack();
+        findPost.updateDesignateStack(updatedPost.getStacks());
+    }
+
+    /***********************
+     * SEARCH POST METHODS *
+     ***********************/
+
+    @Transactional(readOnly = true)
+    public List<Posts> findAll() {
+        return postRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Posts> findPostByUserId(Long userId) {
+        return postRepository.findPostByUserId(userId);
+    }
+
+
+    /***********************
+     * POST UPDATE METHODS *
+     ***********************/
+    @Transactional
+    public void updateDesignateStacks(Long postId, TechStack...techStacks) {
+        Posts findPost = postRepository.findOne(postId);
+        findPost.initDesignateStack();
+        for(TechStack stack : techStacks) {
+            findPost.addDesignateStack(stack);
+        }
+    }
+
 }
--- src/main/java/Tamanegiseoul/comeet/service/StackRelationService.java
+++ src/main/java/Tamanegiseoul/comeet/service/StackRelationService.java
@@ -1,16 +1,40 @@
 package Tamanegiseoul.comeet.service;
+import Tamanegiseoul.comeet.domain.StackRelation;
+import Tamanegiseoul.comeet.domain.enums.TechStack;
+import Tamanegiseoul.comeet.repository.StackRelationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class StackRelationService {
 
+    private final StackRelationRepository stackRelationRepository;
+
     @PersistenceContext
     EntityManager em;
 
+    public List<TechStack> findTechStackByPostId(Long postId) {
+        List<StackRelation> findSR = stackRelationRepository.findByPostId(postId);
+        List<TechStack> findTS = new ArrayList<>();
+        findSR.stream().map(e -> findTS.add(e.getTechStack()));
+        return findTS;
+    }
 
+    public List<TechStack> findTechStackByUserId(Long userId) {
+        List<StackRelation> findSR = stackRelationRepository.findByUserId(userId);
+        List<TechStack> findTS = new ArrayList<>();
+        findSR.stream().map(e -> findTS.add(e.getTechStack()));
+        return findTS;
+    }
+
+    public int removeAllTechStacksByPostId(Long postId) {
+        return stackRelationRepository.removeRelatedStacksByPost(postId);
+    }
 }
