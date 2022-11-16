9e0346eef95bd5927cba5d282cd56061bfb0c48b
Joonwoo Park <93jpark@gmail.com>
2022-11-16 15:41:56 +0900

    specify Exception message with related data type and its value

--- src/main/java/Tamanegiseoul/comeet/domain/exception/DuplicateResourceException.java
+++ src/main/java/Tamanegiseoul/comeet/domain/exception/DuplicateResourceException.java
@@ -9,7 +9,7 @@ public class DuplicateResourceException extends RuntimeException {
     private String fieldValue;
 
     public DuplicateResourceException(String valueType, String fieldValue) {
-        super(String.format("%s's %s is already exists", fieldValue, fieldValue));
+        super(String.format("[%s] '%s' is already exists", valueType, fieldValue));
         this.fieldValue = fieldValue;
         this.valueType = valueType;
     }
