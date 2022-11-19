37f989a8086651db21ae041f5acb614f96b5b8f3
Joonwoo Park <93jpark@gmail.com>
2022-11-19 13:39:30 +0900

    implemented DeleteMapping method to remove user

--- src/main/java/Tamanegiseoul/comeet/api/UserApiController.java
+++ src/main/java/Tamanegiseoul/comeet/api/UserApiController.java
@@ -3,23 +3,27 @@ package Tamanegiseoul.comeet.api;
 import Tamanegiseoul.comeet.domain.Users;
 import Tamanegiseoul.comeet.domain.enums.TechStack;
 import Tamanegiseoul.comeet.domain.exception.DuplicateResourceException;
+import Tamanegiseoul.comeet.domain.exception.ResourceNotFoundException;
 import Tamanegiseoul.comeet.dto.ApiResponse;
 import Tamanegiseoul.comeet.dto.ResponseMessage;
 import Tamanegiseoul.comeet.dto.StatusCode;
 import Tamanegiseoul.comeet.dto.user.request.JoinUserRequest;
+import Tamanegiseoul.comeet.dto.user.request.RemoveUserRequest;
+import Tamanegiseoul.comeet.dto.user.request.SearchUserRequest;
+import Tamanegiseoul.comeet.dto.user.request.UpdateUserRequest;
 import Tamanegiseoul.comeet.dto.user.response.JoinUserResponse;
+import Tamanegiseoul.comeet.dto.user.response.SearchUserResponse;
+import Tamanegiseoul.comeet.dto.user.response.UpdateUserResponse;
 import Tamanegiseoul.comeet.service.CommentService;
 import Tamanegiseoul.comeet.service.PostService;
 import Tamanegiseoul.comeet.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -50,9 +54,9 @@ public class UserApiController {
 
             List<TechStack> preferredStacks = userService.findPreferredStacks(newUser.getId());
 
-            return ApiResponse.of(HttpStatus.OK, ResponseMessage.JOINED_NEW_USER,
+            return ApiResponse.of(HttpStatus.OK, ResponseMessage.CREATED_USER,
                     JoinUserResponse.builder()
-                            .id(newUser.getId())
+                            .userId(newUser.getId())
                             .email(newUser.getEmail())
                             .nickname(newUser.getNickname())
                             .preferredStacks(preferredStacks)
@@ -62,4 +66,56 @@ public class UserApiController {
             return ApiResponse.of(HttpStatus.FORBIDDEN, ResponseMessage.DUPLICATE_RES, e.getMessage());
         }
     }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity<ApiResponse> removeUser(@RequestBody @Valid RemoveUserRequest request) {
+        try {
+            userService.removeUser(request.getUserId());
+            return ApiResponse.of(HttpStatus.OK, ResponseMessage.DELETE_USER, request.getUserId());
+        } catch (ResourceNotFoundException e) {
+            return ApiResponse.of(HttpStatus.NOT_FOUND, ResponseMessage.NOT_FOUND_USER, request.getUserId());
+        }
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<ApiResponse> searchUser(@RequestBody @Valid SearchUserRequest request) {
+        try {
+            Users findUser = userService.findUserById(request.getUserId());
+
+            List<TechStack> preferredStacks = userService.findPreferredStacks(findUser.getId());
+
+            return ApiResponse.of(HttpStatus.OK, ResponseMessage.FOUND_USER, SearchUserResponse.builder()
+                    .id(findUser.getId())
+                    .email(findUser.getEmail())
+                    .nickname(findUser.getNickname())
+                    .stacks(preferredStacks)
+                    .build());
+        } catch (ResourceNotFoundException e) {
+            return ApiResponse.of(HttpStatus.NOT_FOUND, ResponseMessage.NOT_FOUND_USER, e.getMessage());
+        }
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<ApiResponse> updateUser(@RequestBody @Valid UpdateUserRequest request) {
+        try {
+            Users updatedUser = userService.updateUser(request);
+
+            List<TechStack> preferredStacks = userService.findPreferredStacks(updatedUser.getId());
+
+            return ApiResponse.of(HttpStatus.OK, ResponseMessage.UPDATE_USER,
+                    UpdateUserResponse.builder()
+                            .userId(request.getUserId())
+                            .nickname(updatedUser.getNickname())
+                            .email(updatedUser.getEmail())
+                            .preferredStacks(preferredStacks)
+                            .build()
+                    );
+        } catch (ResourceNotFoundException e) {
+            return ApiResponse.of(HttpStatus.NOT_FOUND, e.getMessage());
+        } catch (DuplicateResourceException e) {
+            return ApiResponse.of(HttpStatus.BAD_REQUEST, ResponseMessage.DUPLICATE_RES, e.getMessage());
+        }
+    }
+
+
 }
