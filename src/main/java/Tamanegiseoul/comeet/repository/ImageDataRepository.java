ffc625eddc6eca82d3fed8afa8cf777e018bc27a
Joonwoo Park <93jpark@gmail.com>
2022-11-30 13:53:22 +0900

    made some changed on JPQL query and set return null if not exists

--- src/main/java/Tamanegiseoul/comeet/repository/ImageDataRepository.java
+++ src/main/java/Tamanegiseoul/comeet/repository/ImageDataRepository.java
@@ -13,17 +13,25 @@ import java.util.Optional;
 public class ImageDataRepository {
     private final EntityManager em;
 
-    public void save(ImageData file) {
+    public ImageData save(ImageData file) {
         em.persist(file);
+        return file;
     }
 
     public ImageData findOne(Long id) {
         return em.find(ImageData.class, id);
     }
 
-    public Optional<ImageData> findByName(String name) {
-        return Optional.ofNullable(em.createQuery("select i from ImageData i where i.name = :name", ImageData.class)
-                .setParameter("name", name)
-                .getSingleResult());
+    public ImageData findByUserId(Long userId) {
+        return em.createQuery("select i from ImageData i where i.owner.userId = :userId", ImageData.class)
+                .setParameter("userId", userId)
+                .getResultStream().findFirst().orElse(null);
     }
+
+    public int removeByUserId(Long userId) {
+        return em.createQuery("delete from ImageData i where i.owner.userId = :userId")
+                .setParameter("userId", userId)
+                .executeUpdate();
+    }
+
 }
