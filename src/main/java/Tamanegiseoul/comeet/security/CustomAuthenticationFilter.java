c58032c70b4437cb452cf874dbaa084654452c78
Joonwoo Park <93jpark@gmail.com>
2022-12-12 01:04:59 +0900

    overrided obtain username,password & implemented json parser for servlet request

--- src/main/java/Tamanegiseoul/comeet/security/CustomAuthenticationFilter.java
+++ src/main/java/Tamanegiseoul/comeet/security/CustomAuthenticationFilter.java
@@ -1,22 +1,30 @@
 package Tamanegiseoul.comeet.security;
 
+import Tamanegiseoul.comeet.dto.user.request.LoginUserRequest;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.StreamUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,19 +37,55 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
      // in UsernamePasswordAuthenticationFilter, DEFAULT_ANT_PATH_REQUEST_MATCHER defined url for login as (POST, "/login")
 
     private final AuthenticationManager authenticationManager;
+    private HashMap<String, String> jsonRequest;
 
     public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
     }
 
     @Override
+    protected String obtainPassword(HttpServletRequest request) {
+        String password = super.getPasswordParameter();
+        if(request.getHeader("Content-Type").equals(APPLICATION_JSON_VALUE)) {
+            return jsonRequest.get(password);
+        }
+        return request.getParameter(password);
+    }
+
+    @Override
+    protected String obtainUsername(HttpServletRequest request) {
+        // co-meet will use email as username for login
+        super.setUsernameParameter("email");
+        String email = super.getUsernameParameter();
+        if(request.getHeader("Content-Type").equals(APPLICATION_JSON_VALUE)) {
+            return jsonRequest.get(email);
+        }
+        return request.getParameter(email);
+    }
+
+    @SneakyThrows
+    @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        log.info("Email is : {}", email);
-        log.info("Password is : {}", password);
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email, password);
-        return authenticationManager.authenticate(authenticationToken);
+
+        if(request.getHeader("Content-Type").equals(APPLICATION_JSON_VALUE)) {
+            log.info("JSON LOGIN ATTEMP");
+
+            ObjectMapper om = new ObjectMapper();
+            try {
+                this.jsonRequest = om.readValue(request.getReader().lines().collect(Collectors.joining()),
+                        new TypeReference<HashMap<String, String>>() {});
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new AuthenticationServiceException("Request Content-Type(application/json) Parsing Error");
+            }
+        }
+        String username = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        log.info("{} attempt to login with {}", username, password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        Authentication authenticate = authenticationManager.authenticate(authenticationToken);
+        return authenticate;
     }
 
     @Override
@@ -70,8 +114,6 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         response.setContentType(APPLICATION_JSON_VALUE);
         new ObjectMapper().writeValue(response.getOutputStream(), tokens);
 
-
     }
-
 
 }
