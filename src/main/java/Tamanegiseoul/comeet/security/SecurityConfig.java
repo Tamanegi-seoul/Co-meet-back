54f64425ca446f2050235ce844aff83dd8a1f530
Joonwoo Park <93jpark@gmail.com>
2022-12-13 14:35:44 +0900

    permit all http method options to prevent CORS policy issue related with preflight request

--- src/main/java/Tamanegiseoul/comeet/security/SecurityConfig.java
+++ src/main/java/Tamanegiseoul/comeet/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package Tamanegiseoul.comeet.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         customAuthenticationFilter.setFilterProcessesUrl("/api/login"); // overwrite default url
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**/*").permitAll();
         http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh/**").permitAll();
         http.authorizeRequests().antMatchers("/api/user/validate").permitAll();
         http.authorizeRequests().antMatchers("/api/user/join").permitAll();
