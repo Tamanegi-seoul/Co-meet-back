6bb520828022f35025bb977568f9130b8d649cd8
Joonwoo Park <93jpark@gmail.com>
2022-12-17 21:07:13 +0900

    set UpdateCommentResponse DTO

--- src/main/java/Tamanegiseoul/comeet/dto/comment/response/CommentDto.java
+++ src/main/java/Tamanegiseoul/comeet/dto/comment/response/CommentDto.java
@@ -3,6 +3,7 @@ package Tamanegiseoul.comeet.dto.comment.response;
 import Tamanegiseoul.comeet.domain.Comment;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -13,12 +14,19 @@ import java.time.LocalDateTime;
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class CommentDto {
+    @ApiParam(value = "덧글의 포스트 ID")
     private Long postId;
+    @ApiParam(value = "덧글 ID")
     private Long commentId;
+    @ApiParam(value = "덧글 작성자 ID")
     private Long commenterId;
+    @ApiParam(value = "덧글 작성자 닉네임")
     private String commenterNickname;
+    @ApiParam(value = "덧글 내용")
     private String content;
+    @ApiParam(value = "덧글 생성 시간")
     private LocalDateTime createdTime;
+    @ApiParam(value = "최근 덧글 수정 시간")
     private LocalDateTime modifiedTime;
 
     public static CommentDto toDto(Comment comment) {
--- src/main/java/Tamanegiseoul/comeet/dto/comment/response/UpdateCommentResponse.java
+++ src/main/java/Tamanegiseoul/comeet/dto/comment/response/UpdateCommentResponse.java
@@ -1,13 +1,45 @@
 package Tamanegiseoul.comeet.dto.comment.response;
 
+import Tamanegiseoul.comeet.domain.Comment;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiParam;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Builder
 @Data
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UpdateCommentResponse {
+    @ApiParam(value = "덧글 ID")
+    private Long commentId;
+    @ApiParam(value = "덧글 포스트 ID")
+    private Long postId;
+    @ApiParam(value = "포스트 제목")
+    private String postTitle;
+    @ApiParam(value = "덧글 작성자 ID")
+    private Long commenterId;
+    @ApiParam(value = "덧글 작성자 닉네임")
+    private String commenterNickname;
+    @ApiParam(value = "덧글 내용")
+    private String content;
+    @ApiParam(value = "덧글 작성 시간")
+    private LocalDateTime createdTime;
+    @ApiParam(value = "최근 덧글 수정 시간")
+    private LocalDateTime modifiedTime;
 
+    public static UpdateCommentResponse toDto(Comment comment) {
+        return UpdateCommentResponse.builder()
+                .commentId(comment.getCommentId())
+                .postId(comment.getPost().getPostId())
+                .postTitle(comment.getPost().getTitle())
+                .commenterId(comment.getMember().getMemberId())
+                .commenterNickname(comment.getMember().getNickname())
+                .content(comment.getContent())
+                .createdTime(comment.getCreatedTime())
+                .modifiedTime(comment.getModifiedTime())
+                .build();
+    }
 }
