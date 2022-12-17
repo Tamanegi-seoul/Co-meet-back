dbbcdc5e9de3c111273bda18c7b4446797cd5cc4
Joonwoo Park <93jpark@gmail.com>
2022-12-17 21:01:13 +0900

    added tech stack

--- src/main/java/Tamanegiseoul/comeet/domain/enums/TechStack.java
+++ src/main/java/Tamanegiseoul/comeet/domain/enums/TechStack.java
@@ -1,10 +1,41 @@
 package Tamanegiseoul.comeet.domain.enums;
 
+import org.hibernate.sql.ordering.antlr.GeneratedOrderByFragmentParser;
+
 public enum TechStack {
-    SPRING,
-    REACT,
     JAVA,
     JAVA_SCRIPT,
+    TYPE_SCRIPT,
     PYTHON,
-    R
+    R,
+    C,
+    CPP,
+    GO,
+    SWIFT,
+    KOTLIN,
+    MYSQL,
+    MONGO_DB,
+    PHP,
+    FLUTTER,
+
+    SPRING,
+    REACT,
+    REACT_NATIVE,
+    VUE,
+    NODE_JS,
+    NEXT_JS,
+    NEST_JS,
+    EXPRESS,
+    DJANGO,
+
+    GRAPH_QL,
+    FIREBASE,
+    UNITY,
+    AWS,
+    KUBERNETES,
+    DOCKER,
+    GIT,
+    FIGMA,
+    ZEPLIN
+
 }
