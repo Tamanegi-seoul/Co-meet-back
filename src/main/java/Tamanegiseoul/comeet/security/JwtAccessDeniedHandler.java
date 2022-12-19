1d1201dbaf6a48b95b109a531cb24806b519d3f6
Joonwoo Park <93jpark@gmail.com>
2022-12-19 13:50:06 +0900

    implemented Handler & EntryPoint for exception handling for auth

--- src/main/java/Tamanegiseoul/comeet/security/JwtAccessDeniedHandler.java
+++ src/main/java/Tamanegiseoul/comeet/security/JwtAccessDeniedHandler.java
@@ -1,2 +1,32 @@
-package Tamanegiseoul.comeet.security;public class JwtAccessDeniedHandler {
+package Tamanegiseoul.comeet.security;
+
+import Tamanegiseoul.comeet.dto.StatusCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(StatusCode.FORBIDDEN);
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error_message", accessDeniedException.getMessage());
+        error.put("debug_message", "AuthEntryPoint: 인증실패");
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), error);
+    }
 }
--- src/main/java/Tamanegiseoul/comeet/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/Tamanegiseoul/comeet/security/JwtAuthenticationEntryPoint.java
@@ -1,2 +1,38 @@
-package Tamanegiseoul.comeet.security;public class JwtAuthenticationEntryPoint {
+package Tamanegiseoul.comeet.security;
+
+import Tamanegiseoul.comeet.dto.StatusCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+@Slf4j
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.debug("[JwtAuthenticationEntryPoint:comment]method executed");
+        response.setCharacterEncoding("utf-8");
+        response.setStatus(StatusCode.UNAUTHORIZED);
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        Map<String, String> error = new HashMap<>();
+        error.put("error_message", authException.getMessage());
+        error.put("debug_message", "AuthEntryPoint: 인증실패");
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), error);
+    }
+
 }
