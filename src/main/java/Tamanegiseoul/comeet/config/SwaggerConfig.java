126c413a7fd3355f039ce5214bdd66c7deca811b
Joonwoo Park <93jpark@gmail.com>
2022-12-23 14:32:52 +0900

    edited swagger configuration

--- src/main/java/Tamanegiseoul/comeet/config/SwaggerConfig.java
+++ src/main/java/Tamanegiseoul/comeet/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package Tamanegiseoul.comeet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,12 +10,15 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Configuration
-public class SwaggerConfig {
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
 
     @Bean
     public Docket api() { // Docket: swagger 설정의 핵심이 되는 Bean
