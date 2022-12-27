d190e3818f502843f8d291acebd23123a96dab5a
Joonwoo Park <93jpark@gmail.com>
2022-12-27 16:40:19 +0900

    added if statement for image data not exists

--- src/main/java/Tamanegiseoul/comeet/dto/member/response/ImageDto.java
+++ src/main/java/Tamanegiseoul/comeet/dto/member/response/ImageDto.java
@@ -18,6 +18,10 @@ public class ImageDto {
     private byte[] imageData;
 
     public static ImageDto toDto(ImageData file) {
+        // 등록된 이미지가 없는 경우, null 반환
+        if(file==null)
+            return null;
+        // 등록된 이미지가 있는 경우, 넘겨받은 데이터 기반으로 DTO 생성
         return ImageDto.builder()
                 .imageId(file.getImageId())
                 .fileName(file.getFileName())
