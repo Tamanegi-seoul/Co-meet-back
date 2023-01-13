54fa6fd7606e8aba95f15c83e2ffd309d2d06c1b
Joonwoo Park <93jpark@gmail.com>
2023-01-13 14:08:51 +0900

    refactored member register method with business logics among controller requests

--- src/main/java/Tamanegiseoul/comeet/service/MemberService.java
+++ src/main/java/Tamanegiseoul/comeet/service/MemberService.java
@@ -6,7 +6,10 @@ import Tamanegiseoul.comeet.domain.StackRelation;
 import Tamanegiseoul.comeet.domain.enums.TechStack;
 import Tamanegiseoul.comeet.domain.exception.DuplicateResourceException;
 import Tamanegiseoul.comeet.domain.exception.ResourceNotFoundException;
+import Tamanegiseoul.comeet.dto.member.request.JoinMemberRequest;
 import Tamanegiseoul.comeet.dto.member.request.UpdateMemberRequest;
+import Tamanegiseoul.comeet.dto.member.response.ImageDto;
+import Tamanegiseoul.comeet.dto.member.response.JoinMemberResponse;
 import Tamanegiseoul.comeet.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,9 +21,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.io.IOException;
 import java.util.*;
 
 @Service @Slf4j
@@ -36,39 +41,56 @@ public class MemberService implements UserDetailsService {
 
     private final PostService postService;
 
+    private final ImageDataService imageDataService;
+
     private final PasswordEncoder passwordEncoder;
 
     @PersistenceContext
     private final EntityManager em;
 
     @Transactional
-    public Long registerMember(Member member) {
-        log.info("register new member {} to the database", member.getNickname());
-        validateMemberEmail(member.getEmail());
-        validateMemberNickname(member.getNickname());
-        member.changePassword(passwordEncoder.encode(member.getPassword())); // encrypt password
-        member.updateCreatedDate();
-        member.updateModifiedDate();
-        memberRepository.save(member);
-        return member.getMemberId();
+    public JoinMemberResponse registerMember(JoinMemberRequest request, MultipartFile image) throws IOException {
+
+        validateMemberEmail(request.getEmail());
+        validateMemberNickname(request.getNickname());
+
+        Member newMember = Member.builder()
+                .email(request.getEmail())
+                .nickname(request.getNickname())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+
+
+        newMember.updateCreatedDate();
+        newMember.updateModifiedDate();
+        memberRepository.save(newMember);
+
+        this.addRoleToMember(newMember.getEmail(), "ROLE_USER");
+        this.updatePreferStack(newMember.getMemberId(), request.getPreferStacks());
+
+        if(image!=null) {
+            ImageDto imageDto = imageDataService.uploadImage(newMember, image);
+
+            return JoinMemberResponse.toDto(newMember).preferStacks(request.getPreferStacks()).profileImage(imageDto);
+        }
+
+
+        return JoinMemberResponse.toDto(newMember).preferStacks(request.getPreferStacks()).profileImage(null);
     }
 
     @Transactional
     public Role saveRole(Role role){
-        log.info("register new role {} to the database", role.getRoleName());
         return roleRepository.save(role);
     }
 
     @Transactional
     public void addRoleToMember(String email, String roleName) {
-        log.info("Adding role {} to member {}", roleName, email);
-
-        Member member = memberRepository.findMemberByEmail(email);
-        Role role = roleRepository.findByRoleName(roleName);
-        if(role == null) {
+        Member findMember = memberRepository.findMemberByEmail(email);
+        Role roleToAdd = roleRepository.findByRoleName(roleName);
+        if(roleToAdd == null) {
             log.warn("no such role {}", roleName);
         } else {
-            member.getRoles().add(role);
+            findMember.addRole(roleToAdd);
         }
     }
 
@@ -101,8 +123,6 @@ public class MemberService implements UserDetailsService {
         Member findMember = this.findMemberById(request.getMemberId());
         Long findMemberId = findMember.getMemberId();
         findMember.changeNickname(request.getNewNickname());
-        //findMember.changePassword(passwordEncoder.encode(request.getNewPassword()));
-        //findMember.initPreferredTechStacks();
         this.updatePreferStack(findMemberId, request.getUpdatedStacks());
         findMember.updateModifiedDate();
         em.flush();
@@ -129,8 +149,7 @@ public class MemberService implements UserDetailsService {
     public void updatePreferStack(Long memberId, List<TechStack> techStacks) {
         log.warn("[MemberService:updatePreferStack] method init");
         Member findMember = this.findMemberById(memberId); // checked
-        //findMember.initPreferredTechStacks();
-        stackRelationRepository.removeRelatedStakcsByMember(memberId);
+        findMember.clearPreferStack();
         for(TechStack ts : techStacks) {
             findMember.addPreferStack(ts);
         }
@@ -141,19 +160,12 @@ public class MemberService implements UserDetailsService {
     }
 
     @Transactional
-    public int removeMember(Long memberId) throws ResourceNotFoundException {
+    public Long removeMember(Long memberId) throws ResourceNotFoundException {
         Member findMember = this.findMemberById(memberId);
 
-        // first of all,
-        // delete related child entities
-        stackRelationRepository.removeRelatedStakcsByMember(memberId);
-        commentRepository.removeCommentBymemberId(memberId);
-        imageDataRepository.removeImageByMemberId(memberId);
-        //postRepository.removePostByPosterId(memberId);
-        postService.removePostByPosterId(memberId);
+        em.remove(findMember);
 
-        // then, remove parent entity
-        return memberRepository.removeByMemberId(memberId);
+        return findMember.getMemberId();
     }
 
     /**********************
@@ -165,7 +177,7 @@ public class MemberService implements UserDetailsService {
         Member findMember = memberRepository.findOne(memberId);
 
         if(findMember == null) {
-            log.info("[MemberService:findMemberById] THE RESULT OF FIND ONE is NULL ");
+            log.info("[MemberService:findMemberById] THE RESULT OF MEMBER ID is NULL ");
             throw new ResourceNotFoundException("member_id", "memberId ", memberId);
         } else {
             log.info("[MemberService:findMemberById] find user with provided member id : " + memberId);
