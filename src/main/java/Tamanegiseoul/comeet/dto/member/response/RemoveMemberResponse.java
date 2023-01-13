182412c4caf442274786f82a8bc6a19e8d1f72b7
Joonwoo Park <93jpark@gmail.com>
2023-01-13 17:28:35 +0900

    implemented toDto() for transfer entity to dto

--- src/main/java/Tamanegiseoul/comeet/dto/member/response/RemoveMemberResponse.java
+++ src/main/java/Tamanegiseoul/comeet/dto/member/response/RemoveMemberResponse.java
@@ -1,5 +1,6 @@
 package Tamanegiseoul.comeet.dto.member.response;
 
+import Tamanegiseoul.comeet.domain.Member;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
@@ -11,4 +12,11 @@ import lombok.Data;
 public class RemoveMemberResponse {
     private Long memberId;
     private String nickname;
+
+    public static RemoveMemberResponse toDto(Member member) {
+        return RemoveMemberResponse.builder()
+                .memberId(member.getMemberId())
+                .nickname(member.getNickname())
+                .build();
+    }
 }
--- src/main/java/Tamanegiseoul/comeet/dto/member/response/SearchMemberResponse.java
+++ src/main/java/Tamanegiseoul/comeet/dto/member/response/SearchMemberResponse.java
@@ -1,18 +1,20 @@
 package Tamanegiseoul.comeet.dto.member.response;
 
+import Tamanegiseoul.comeet.domain.Member;
 import Tamanegiseoul.comeet.domain.enums.TechStack;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
-@Builder
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
+@NoArgsConstructor
 public class SearchMemberResponse {
     private Long memberId;
     private String email;
@@ -23,4 +25,27 @@ public class SearchMemberResponse {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedTime;
     private ImageDto profileImage;
+
+    @Builder
+    public SearchMemberResponse(Long memberId, String email, String nickname, List<TechStack> preferStacks, LocalDateTime createdTime, LocalDateTime modifiedTime, ImageDto profileImage) {
+        this.memberId = memberId;
+        this.email = email;
+        this.nickname = nickname;
+        this.preferStacks = preferStacks;
+        this.createdTime = createdTime;
+        this.modifiedTime = modifiedTime;
+        this.profileImage = profileImage;
+    }
+
+    public static SearchMemberResponse toDto(Member member, ImageDto image, List<TechStack> stacks) {
+        return SearchMemberResponse.builder()
+                .memberId(member.getMemberId())
+                .email(member.getEmail())
+                .nickname(member.getNickname())
+                .preferStacks(stacks)
+                .createdTime(member.getCreatedTime())
+                .modifiedTime(member.getModifiedTime())
+                .profileImage(image)
+                .build();
+    }
 }
