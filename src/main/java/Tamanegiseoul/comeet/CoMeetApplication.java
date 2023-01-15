3755512b558979840076fd51cedcbc77b95d0552
Joonwoo Park <93jpark@gmail.com>
2023-01-15 17:58:58 +0900

    set configuration for web mvc

--- src/main/java/Tamanegiseoul/comeet/CoMeetApplication.java
+++ src/main/java/Tamanegiseoul/comeet/CoMeetApplication.java
@@ -10,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class CoMeetApplication {
 
