e7d0098d47e87edeff4ffdeba5d3b94f4ee569ea
Joonwoo Park <93jpark@gmail.com>
2023-01-16 15:14:48 +0900

    removed inappropriate annotation

--- src/main/java/Tamanegiseoul/comeet/CoMeetApplication.java
+++ src/main/java/Tamanegiseoul/comeet/CoMeetApplication.java
@@ -1,18 +1,11 @@
 package Tamanegiseoul.comeet;
 
-import Tamanegiseoul.comeet.domain.Member;
-import Tamanegiseoul.comeet.domain.Role;
-import Tamanegiseoul.comeet.service.MemberService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
 @SpringBootApplication
 public class CoMeetApplication {
 
