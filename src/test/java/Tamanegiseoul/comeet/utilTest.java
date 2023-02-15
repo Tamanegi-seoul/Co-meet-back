c3b14db0cd96c517fce3f1451bcf388444d55fa6
Joonwoo Park <93jpark@gmail.com>
2023-02-15 14:14:29 +0900

    clear code format

--- src/test/java/Tamanegiseoul/comeet/utilTest.java
+++ src/test/java/Tamanegiseoul/comeet/utilTest.java
@@ -1,9 +1,12 @@
 package Tamanegiseoul.comeet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
