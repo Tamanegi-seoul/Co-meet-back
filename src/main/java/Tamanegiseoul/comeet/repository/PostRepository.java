e7ec243c5cba34d0fe489c218164e6f3ad1f9ce6
Joonwoo Park <93jpark@gmail.com>
2023-02-19 16:39:10 +0900

    implement find post with stack by join fetch.

--- src/main/java/Tamanegiseoul/comeet/repository/PostRepository.java
+++ src/main/java/Tamanegiseoul/comeet/repository/PostRepository.java
@@ -19,6 +19,11 @@ public class PostRepository {
         return em.find(Posts.class, id);
     }
 
+    public Posts findPostWithStack(Long postId) {
+        return em.createQuery("select p from Posts p join fetch p.designatedStack where p.postId = :postId", Posts.class)
+                .setParameter("postId", postId)
+                .getSingleResult();
+    }
     public List<Posts> findAll() {
         return em.createQuery("select p from Posts p", Posts.class)
                 .getResultList();
