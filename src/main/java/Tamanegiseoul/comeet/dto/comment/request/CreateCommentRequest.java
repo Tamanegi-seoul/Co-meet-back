10438668dd48bdef1ffd3ba07d12c4a81b03d563
Joonwoo Park <93jpark@gmail.com>
2023-02-21 00:47:35 +0900

    Put annotation for swagger model property descriptions.

--- src/main/java/Tamanegiseoul/comeet/dto/comment/request/CreateCommentRequest.java
+++ src/main/java/Tamanegiseoul/comeet/dto/comment/request/CreateCommentRequest.java
@@ -2,21 +2,30 @@ package Tamanegiseoul.comeet.dto.comment.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 @NoArgsConstructor
 public class CreateCommentRequest {
-    @Schema(description = "작성할 덧글의 포스트 ID", example = "5")
+
+    @NonNull
+    @ApiModelProperty(notes="게시글 ID", example="12", required=true)
     private Long postId;
-    @Schema(description = "덧글 작성자 ID", example = "3")
+
+    @NonNull
+    @ApiModelProperty(notes="덧글 작성자 ID", example="6", required=true)
     private Long memberId;
-    @Schema(description = "적성할 덧글 내용", example = "예시 덧글입니다.")
+
+    @NonNull
+    @ApiModelProperty(notes="적성할 덧글 내용", example="예시 덧글입니다.", required=true)
     private String content;
 
     @Builder
--- src/main/java/Tamanegiseoul/comeet/dto/comment/response/SearchCommentResponse.java
+++ src/main/java/Tamanegiseoul/comeet/dto/comment/response/SearchCommentResponse.java
@@ -3,12 +3,14 @@ package Tamanegiseoul.comeet.dto.comment.response;
 import Tamanegiseoul.comeet.domain.Comment;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +23,8 @@ import java.util.stream.Collectors;
 @JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
 public class SearchCommentResponse {
 
+    @Nullable
+    @ApiModelProperty(notes="덧글 리스트")
     private List<CommentDto> commentList;
 
 }
